1112fd7e244c3145cecfe3e648264170d2e4ebdc
SugarSyrup <tlfvm04@naver.com>
2024-01-23 22:42:55 +0900

    fix : ThemeProvider Color 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,23 @@
 const theme = {
     white: "#FFFFF",
-    lightGray: "#F6F6F6",
-    gray: "#D3D3D3",
-    normalGray: "#ADADAD",
-    darkGray: "#727272",
     black: "#121212",
-    primary: "#5276FA",
-    primaryBlack: "#3C3C43",
-    alertRed: "#FF6060",
+    red: "#FF6060",
+
+    main: "#5276FA",
+    blue01: "#4969E0",
+    blue02: "#849FFF",
+    blue03: "#ACBAF3",
+    blue04: "#D4DDFF",
+    blue05: "#F3F6FF",
+
+    gray: "#424242",
+    gray01: "#727272",
+    gray02: "#ADADAD",
+    gray03: "#D3D3D3",
+    gray04: "#EBEBEB",
+    gray05: "#F3F4F6",
+    gray06: "#F6F6F6",
+    gray07: "#FDFDFD",
 };
 
 export default theme;
