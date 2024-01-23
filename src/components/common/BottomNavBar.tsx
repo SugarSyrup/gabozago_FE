522d6b108e6d579dd6365dbc51e7126e7a1f2285
여다희 <yeodahui.dev@gmail.com>
2024-01-24 01:25:53 +0900

    feat(#1): 하단 네비게이션 바 퍼블리싱

--- src/components/common/BottomNavBar.tsx
+++ src/components/common/BottomNavBar.tsx
@@ -1,23 +1,49 @@
+import { ReactNode } from "react";
+import HomeIcon from "../../assets/icons/home.svg?react";
+import MyIcon from "../../assets/icons/my.svg?react";
+import ScrapIcon from "../../assets/icons/scrap.svg?react";
+import TripIcon from "../../assets/icons/trip.svg?react";
 import * as S from "../../styles/common/BottomNavBar.style";
+import { Link } from "react-router-dom";
 
-type NavItemType = "홈" | "내 여행" | "스크랩" | "MY";
-
-interface Props {
-  location: NavItemType;
+interface TabProps {
+  title: string;
+  active?: boolean;
+  path?: string;
+  icon?: ReactNode;
+}
+function Tab({ active, title, path = "", icon }: TabProps) {
+  return (
+    <S.ListItem $active={active}>
+      <Link to={path}>
+        {icon}
+        <span>{title}</span>
+      </Link>
+    </S.ListItem>
+  );
 }
 
-function BottomNavBar({ location = "홈" }: Props) {
-  const navItems: NavItemType[] = ["홈", "내 여행", "스크랩", "MY"];
+function BottomNavBar({ location = "홈" }) {
+  /**
+   * @todo 라우트 변경 시 path 옵션 수정이 필요합니다.
+   */
+  const navItems = [
+    { title: "홈", icon: <HomeIcon />, path: "" },
+    { title: "내 여행", icon: <TripIcon />, path: "" },
+    { title: "스크랩", icon: <ScrapIcon />, path: "" },
+    { title: "MY", icon: <MyIcon />, path: "" },
+  ];
+
   return (
     <S.Nav>
       <S.NavList>
-        {navItems.map((item: string) => (
-          <S.ListItem className={`${location === item && "active"}`}>
-            <a>
-              (아이콘)
-              <span>{item}</span>
-            </a>
-          </S.ListItem>
+        {navItems.map((item) => (
+          <Tab
+            title={item.title}
+            active={location === item.title ? true : false}
+            icon={item.icon}
+            path={item.path}
+          />
         ))}
       </S.NavList>
     </S.Nav>
--- src/styles/common/BottomNavBar.style.ts
+++ src/styles/common/BottomNavBar.style.ts
@@ -7,21 +7,45 @@ export const Nav = styled.nav`
   left: 0;
   right: 0;
   z-index: 20;
+  background-color: ${({ theme }) => theme.white};
 `;
 export const NavList = styled.ul`
   margin: auto;
-  margin-bottom: 19px;
+  margin-bottom: 20px;
   width: 100%;
-  display: flex;
-  justify-content: space-around;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
 `;
-export const ListItem = styled.li`
+
+export const ListItem = styled.li<{ $active?: boolean }>`
   a {
     padding: 12px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    text-decoration: none;
+    color: ${({ $active, theme }) => ($active ? theme.main : theme.gray02)};
+    transition: all ease-in-out 0.3s;
+
+    span {
+      font-size: 10px;
+      font-weight: 600;
+      line-height: 12px;
+      margin-top: 2px;
+    }
+    path {
+      transition: all ease-in-out 0.3s;
+      fill: ${({ $active, theme }) => ($active ? theme.main : theme.gray02)};
+    }
+
+    &:hover {
+      color: ${({ theme }) => theme.main};
+      path {
+        fill: ${({ theme }) => theme.main};
+      }
+    }
   }
 
   /* width: 100%; */
