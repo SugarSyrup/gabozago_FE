68d9178bb61db27ad2247ec74fa7cdeaae97b6b6
여다희 <yeodahui.dev@gmail.com>
2024-01-24 01:41:20 +0900

    fix(#1): Lint 컨벤션에 맞게 파일 하나에 컴포넌트 한 개 형태로 수정

--- src/components/common/BottomNavBar.tsx
+++ src/components/common/BottomNavBar.tsx
@@ -1,27 +1,9 @@
-import { ReactNode } from "react";
+import { Link } from "react-router-dom";
 import HomeIcon from "../../assets/icons/home.svg?react";
 import MyIcon from "../../assets/icons/my.svg?react";
 import ScrapIcon from "../../assets/icons/scrap.svg?react";
 import TripIcon from "../../assets/icons/trip.svg?react";
 import * as S from "../../styles/common/BottomNavBar.style";
-import { Link } from "react-router-dom";
-
-interface TabProps {
-  title: string;
-  active?: boolean;
-  path?: string;
-  icon?: ReactNode;
-}
-function Tab({ active, title, path = "", icon }: TabProps) {
-  return (
-    <S.ListItem $active={active}>
-      <Link to={path}>
-        {icon}
-        <span>{title}</span>
-      </Link>
-    </S.ListItem>
-  );
-}
 
 function BottomNavBar({ location = "홈" }) {
   /**
@@ -38,12 +20,12 @@ function BottomNavBar({ location = "홈" }) {
     <S.Nav>
       <S.NavList>
         {navItems.map((item) => (
-          <Tab
-            title={item.title}
-            active={location === item.title ? true : false}
-            icon={item.icon}
-            path={item.path}
-          />
+          <S.ListItem $active={location === item.title ? true : false}>
+            <Link to={item.path}>
+              {item.icon}
+              <span>{item.title}</span>
+            </Link>
+          </S.ListItem>
         ))}
       </S.NavList>
     </S.Nav>
