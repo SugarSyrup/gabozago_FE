eed9ebf4df150a1246aa47e37d1496bffc56e1be
여다희 <yeodahui.dev@gmail.com>
2024-01-23 23:09:39 +0900

    feat(#1): 페이지 템플릿 컴포넌트 사용 예시 내 여행 일정 페이지 컴포넌트 생성 및 테스트용 라우트 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./pages/App";
+import SchedulePage from "./pages/SchedulePage";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/schedule",
+    element: <SchedulePage />,
+  },
 ]);
 
 export default router;
