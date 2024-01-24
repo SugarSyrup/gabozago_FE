0c926b3091622240c2c3f43139b38321dcbeb2bf
SugarSyrup <tlfvm04@naver.com>
2024-01-24 22:47:09 +0900

    feat(#1): Heading 컴포넌트 생성

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -1,23 +1,27 @@
 import PageTemplate from "../components/common/PageTemplate";
 import useSearchInput from "../hooks/useSearchInput";
 import useModal from "../hooks/useModal";
+import Heading from "../components/common/Heading";
 
 function SchedulePage() {
-  const [, SearchInput] = useSearchInput({});
-  const { Modal, modalOpen } = useModal({
-    title: "제목입력",
-  });
-  
-  return (
-      <PageTemplate nav={true} header={false}>
-        <Modal>
-          <div>TEST</div>
-        </Modal>
-        <SearchInput />
-        여행 일정 페이지
-        <button onClick={modalOpen}>모달 open</button>
-      </PageTemplate>
-  );
+    const [, SearchInput] = useSearchInput({});
+    const { Modal, modalOpen } = useModal({
+        title: "제목입력",
+    });
+
+    return (
+        <PageTemplate nav={true} header={false}>
+            <Modal>
+                <div>TEST</div>
+            </Modal>
+            <Heading size="md" maxWidth={50} noOfLine={4}>
+                즐거운 부산 여행
+            </Heading>
+            <SearchInput />
+            여행 일정 페이지
+            <button onClick={modalOpen}>모달 open</button>
+        </PageTemplate>
+    );
 }
 
 export default SchedulePage;
