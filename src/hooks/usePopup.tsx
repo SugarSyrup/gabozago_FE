5380e17dc870fd7350f8275ce180460f3f64949d
여다희 <yeodahui.dev@gmail.com>
2024-01-24 23:02:38 +0900

    feat(#1): 팝업 사용할 수 있는 usePopup 컴포넌트 생성

---
+++ src/hooks/usePopup.tsx
@@ -0,0 +1,40 @@
+import { MouseEvent, ReactNode, useState } from "react";
+import * as S from "../styles/common/Popup.style";
+
+function usePopup() {
+  const [isOpend, setIsOpend] = useState(false);
+
+  const popupOpen = () => {
+    setIsOpend(true);
+  };
+  const popupClose = () => {
+    setIsOpend(false);
+  };
+
+  interface PopupProps {
+    children: ReactNode;
+    hasHeader?: boolean;
+    title?: string;
+    hasCloseButton?: boolean;
+    padding?: string;
+  }
+  const Popup = ({ children, padding }: PopupProps) => (
+    <>
+      {isOpend && (
+        <S.PopupContainer onClick={popupClose}>
+          <S.Popup
+            padding={padding}
+            onClick={(e: MouseEvent) => {
+              e.stopPropagation();
+            }}
+          >
+            <S.Contents>{children}</S.Contents>
+          </S.Popup>
+        </S.PopupContainer>
+      )}
+    </>
+  );
+  return { popupClose, popupOpen, Popup };
+}
+
+export default usePopup;
--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -1,22 +1,28 @@
 import PageTemplate from "../components/common/PageTemplate";
 import useSearchInput from "../hooks/useSearchInput";
 import useModal from "../hooks/useModal";
+import usePopup from "../hooks/usePopup";
 
 function SchedulePage() {
   const [, SearchInput] = useSearchInput({});
   const { Modal, modalOpen } = useModal({
     title: "제목입력",
   });
-  
+  const { Popup, popupOpen } = usePopup();
+
   return (
-      <PageTemplate nav={true} header={false}>
-        <Modal>
-          <div>TEST</div>
-        </Modal>
-        <SearchInput />
-        여행 일정 페이지
-        <button onClick={modalOpen}>모달 open</button>
-      </PageTemplate>
+    <PageTemplate nav={true} header={false}>
+      <Modal>
+        <div>TEST</div>
+      </Modal>
+      <Popup title="신고하기" padding="30px 40px">
+        신고가 접수되었습니다.
+      </Popup>
+      <SearchInput />
+      여행 일정 페이지
+      <button onClick={modalOpen}>모달 open</button>
+      <button onClick={popupOpen}>팝업 open</button>
+    </PageTemplate>
   );
 }
 
