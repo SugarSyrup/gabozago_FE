30bcd61cfd5cb4ec49586948dd4c5d8cf8b81da7
여다희 <yeodahui.dev@gmail.com>
2024-01-25 20:46:46 +0900

    refactor(#13): 여행 계획 더미데이터 파일 분리

--- src/pages/MyTripDetailPage.tsx
+++ src/pages/MyTripDetailPage.tsx
@@ -4,61 +4,11 @@ import * as S from "../styles/MyTripDetailPage.style";
 import ClapBlueIcon from "../assets/icons/clap_blue.svg?react";
 import TripPlanListPlaceHolder from "../components/tripDetail/TripPlanListPlaceHolder";
 import TripPlanList from "../components/tripDetail/TripPlanList";
+import { data as planData } from "../assets/data/tripPlanData";
 
 function MyTripDetailPage() {
   const username = "최민석";
-  const data = {
-    title: "즐거운 부산 여행",
-    departureDate: new Date("2024-12-20"), // 출발
-    arrivalDate: new Date("2024-12-23"), // 도착
-    days: 4,
-    transport: "대중교통",
-    plan: [
-      {
-        day: 1,
-        date: new Date("2024-12-20"),
-        dayOfWeek: "수",
-        route: [
-          {
-            placeName: "더베이베이커리",
-            theme: "베이커리",
-            placeImage:
-              "https://placehold.co/300x300/738DEC/white?text=go&font=roboto",
-            memo: "",
-            transfort: "",
-            travelTime: "",
-          },
-          {
-            placeName: "장소명",
-            theme: "테마",
-            placeImage:
-              "https://placehold.co/300x300/738DEC/white?text=go&font=roboto",
-            memo: "",
-            transfort: "",
-            travelTime: "15분",
-          },
-        ],
-      },
-      {
-        day: 2,
-        date: new Date("2024-12-21"),
-        dayOfWeek: "목",
-        route: [],
-      },
-      {
-        day: 3,
-        date: new Date("2024-12-22"),
-        dayOfWeek: "목",
-        route: [],
-      },
-      {
-        day: 4,
-        date: new Date("2024-12-23"),
-        dayOfWeek: "토",
-        route: [],
-      },
-    ],
-  };
+  const data = planData;
 
   return (
     <PageTemplate nav={true} header={false}>
