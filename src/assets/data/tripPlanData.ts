c4c4a749ceaa237066a52929c2e93bcab06492f2
여다희 <yeodahui.dev@gmail.com>
2024-01-26 00:23:15 +0900

    fix(#13): Plan 더미데이터 타입스크립트 파일로 변경

--- src/assets/data/tripPlanData.ts
+++ src/assets/data/tripPlanData.ts
@@ -1,3 +1,19 @@
+export interface DayPlan {
+  day: number;
+  date: Date;
+  dayOfWeek: string;
+  route: Place[];
+}
+export interface Place {
+  placeName: string;
+  theme: string;
+  placeId: number;
+  position: { lat: number; lng: number };
+  placeImage: string;
+  memo: string;
+  transfort: string;
+  travelTime: string;
+}
 export const data = {
   title: "즐거운 부산 여행",
   departureDate: new Date("2024-12-20"), // 출발
@@ -13,6 +29,8 @@ export const data = {
         {
           placeName: "더베이베이커리",
           theme: "베이커리",
+          placeId: 1232132131,
+          position: { lat: 37.1234, lng: -122.5678 },
           placeImage:
             "https://placehold.co/300x300/738DEC/white?text=go&font=roboto",
           memo: "",
@@ -22,6 +40,8 @@ export const data = {
         {
           placeName: "장소명",
           theme: "테마",
+          placeId: 1232132131,
+          position: { lat: 37.1234, lng: -122.5678 },
           placeImage:
             "https://placehold.co/300x300/738DEC/white?text=go&font=roboto",
           memo: "",
