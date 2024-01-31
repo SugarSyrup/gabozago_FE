42d4598b112ed95e0d01fb95f76990f1d46257f6
SugarSyrup <tlfvm04@naver.com>
2024-01-31 22:14:01 +0900

    feat(#23) : mock data 생성, 코드 수정

--- src/pages/MyTripCreatePage.tsx
+++ src/pages/MyTripCreatePage.tsx
@@ -1,10 +1,13 @@
-import PageTemplate from "../components/common/PageTemplate";
-import useSearchInput from "../hooks/useSearchInput";
-import LeftChevronIcon from "../assets/icons/leftChevron.svg?react";
 import { useNavigate } from "react-router-dom";
+
+import { locations } from "../assets/data/locations";
+import LeftChevronIcon from "../assets/icons/leftChevron.svg?react";
+import PageTemplate from "../components/common/PageTemplate";
+import Button from "../components/common/Button";
+import useSearchInput from "../hooks/useSearchInput";
+
 import * as S from "../styles/pages/MyTripCreatePage.style";
 import { Heading } from "../styles/common/Heading.style";
-import Button from "../components/common/Button";
 
 function MyTripCreatePage() {
     const navigate = useNavigate();
@@ -27,24 +30,11 @@ function MyTripCreatePage() {
                 <Heading size="md">지역을 선택해주세요.</Heading>
             </S.LocationsHeader>
             <S.Locations>
-                <Button size="md" type="normal">
-                    부산
-                </Button>
-                <Button size="md" type="normal">
-                    부산
-                </Button>
-                <Button size="md" type="normal">
-                    부산
-                </Button>
-                <Button size="md" type="normal">
-                    부산
-                </Button>
-                <Button size="md" type="normal">
-                    부산
-                </Button>
-                <Button size="md" type="normal">
-                    부산
-                </Button>
+                {locations.map((location) => (
+                    <Button size="md" type="normal">
+                        {location}
+                    </Button>
+                ))}
             </S.Locations>
             <S.Footer>
                 <Button size="lg" type="normal" disabled={true}>
