0c4ca67a36da22ed17d3789f6027c752847ed264
SugarSyrup <tlfvm04@naver.com>
2024-02-15 21:42:42 +0900

    feat(#33): 추천 여행리뷰 리스트 컴포넌트 구현

---
+++ src/styles/tripDetail/RecommendationReviewItem.style.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    padding: 12px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.gray05};
+`;
+
+export const LeftItems = styled.div`
+    display: flex;
+    gap: 9px;
+`;
+
+export const Thumbnail = styled.div`
+    width: 44px;
+    height: 44px;
+    border-radius: 100%;
+
+    background-color: ${({ theme }) => theme.gray04};
+`;
+
+export const Infomation = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 2px;
+
+    font-size: 11px;
+    font-weight: 400;
+
+    svg {
+        width: 14px;
+        height: 14px;
+    }
+`;
+
+export const Name = styled.span`
+    font-weight: 600;
+    line-height: 22px;
+    font-size: 14px;
+
+    display: inline-block;
+    max-width: 210px;
+    text-overflow: ellipsis;
+`;
+
+export const Desc = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 7px;
+`;
