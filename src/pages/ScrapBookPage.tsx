6548e48804be9f505390a6e31e721e3c923a6853
여다희 <yeodahui.dev@gmail.com>
2024-02-23 00:26:00 +0900

    feat(#39): 스크랩 북 여행기/여행장소 탭 나누기

--- src/pages/ScrapBookPage.tsx
+++ src/pages/ScrapBookPage.tsx
@@ -1,9 +1,33 @@
-import React from "react";
-import * as S from "../styles/pages/scrapBook/srapBookPage.style";
+import * as S from "../styles/pages/scrapBook/scrapBookPage.style";
 import PageTemplate from "../components/common/PageTemplate";
+import { useState } from "react";
 
 function ScrapBookPage(props) {
-  return <PageTemplate></PageTemplate>;
+  const [tabs, setTabs] = useState([
+    { id: 1, title: "여행기", content: <></> },
+    { id: 2, title: "여행 장소", content: <></> },
+  ]); // content 속성에 보여줄 컴포넌트 매핑
+  const [focusedTabId, setFocusedTabId] = useState(1);
+
+  return (
+    <PageTemplate>
+      <S.Header>
+        <S.Heading>스크랩 북</S.Heading>
+        <S.TabList>
+          {tabs.map((tab) => (
+            <S.TabItem
+              focused={tab.id === focusedTabId}
+              onClick={() => {
+                setFocusedTabId(tab.id);
+              }}
+            >
+              {tab.title}
+            </S.TabItem>
+          ))}
+        </S.TabList>
+      </S.Header>
+    </PageTemplate>
+  );
 }
 
 export default ScrapBookPage;
---
+++ src/styles/pages/scrapBook/scrapBookPage.style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+`;
+
+export const Heading = styled.h1`
+  width: 100%;
+  padding: 40px 10px 10px;
+
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 22px;
+`;
+export const TabList = styled.ol`
+  display: flex;
+  justify-content: space-around;
+`;
+export const TabItem = styled.li<{ focused: boolean }>`
+  padding: 10px;
+  flex-grow: 1;
+
+  cursor: pointer;
+  text-align: center;
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: ${({ focused }) => (focused === true ? "600" : "500")};
+  color: ${({ theme, focused }) =>
+    focused === true ? theme.main : theme.gray01};
+  border-bottom: 2px solid
+    ${({ theme, focused }) => (focused === true ? theme.main : theme.gray04)};
+
+  transition: all 0.3s ease-in-out;
+`;
