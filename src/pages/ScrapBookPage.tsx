497491142e3b0b1baba48faa1ea76ac923dce9f8
여다희 <yeodahui.dev@gmail.com>
2024-02-22 21:49:35 +0900

    feat(#39): 스크랩 페이지 라우트에 추가 및 하단바 하이라이팅 path 추가

---
+++ src/pages/ScrapBookPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import * as S from "../styles/pages/scrapBook/srapBookPage.style";
+import PageTemplate from "../components/common/PageTemplate";
+
+function ScrapBookPage(props) {
+  return <PageTemplate></PageTemplate>;
+}
+
+export default ScrapBookPage;
