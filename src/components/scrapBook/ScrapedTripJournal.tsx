a2d52d3871e1fc34ce5e21b2972e5af50af7dac2
여다희 <yeodahui.dev@gmail.com>
2024-02-23 21:27:23 +0900

    refactor(#39): 사용하지 않는 코드 삭제

--- src/components/scrapBook/ScrapedTripJournal.tsx
+++ src/components/scrapBook/ScrapedTripJournal.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import * as S from "../../styles/scrapBook/ScrapedTripJournal.style";
 import addCircle from "../../assets/icons/add_circle.svg";
 import { useNavigate } from "react-router-dom";
@@ -8,10 +7,6 @@ import { scrapedJournalGroupsState } from "../../recoil/scrapBook/scrapState";
 function ScrapedTripJournal() {
   const navigate = useNavigate();
   const journalGroups = useRecoilValue(scrapedJournalGroupsState);
-
-  useEffect(() => {
-    // user의 여행기 스크랩 목록 가져오기
-  }, []);
 
   return (
     <S.GroupList>
