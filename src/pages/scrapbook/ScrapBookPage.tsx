2329635f77e0e5c13d4fe78273428417c1c9b676
여다희 <yeodahui.dev@gmail.com>
2024-03-04 01:03:41 +0900

    fix(#47): TabBar 커스텀 훅 공통 컴포넌트로 변경

--- src/pages/scrapbook/ScrapBookPage.tsx
+++ src/pages/scrapbook/ScrapBookPage.tsx
@@ -2,20 +2,26 @@ import * as S from "../../styles/pages/scrapBook/ScrapBookPage.style";
 import PageTemplate from "../../components/common/PageTemplate";
 import ScrapedTripJournal from "../../components/scrapBook/ScrapedTripJournal";
 import ScrapedTripPlace from "../../components/scrapBook/ScrapedTripPlace";
-import useTabBar from "../../hooks/useTabBar";
+// import useTabBar from "../../hooks/useTabBar";
+import { useState } from "react";
+import TabBar from "../../components/common/TabBar";
 
 function ScrapBookPage() {
+  const [focusedTabIndex, setFocusedTabIndex] = useState<number>(0);
   const tabs = [
     { id: 1, name: "여행기", content: <ScrapedTripJournal /> },
     { id: 2, name: "여행 장소", content: <ScrapedTripPlace /> },
   ]; // content 속성에 보여줄 컴포넌트 매핑
-  const { TabBar, focusedTabIndex } = useTabBar({ tabs });
 
   return (
     <PageTemplate>
       <S.Header>
         <S.Heading>스크랩 북</S.Heading>
-        <TabBar />
+        <TabBar
+          tabs={tabs}
+          focusedTabIndex={focusedTabIndex}
+          setFocusedTabIndex={setFocusedTabIndex}
+        />
       </S.Header>
       <S.Contents>{tabs[focusedTabIndex].content}</S.Contents>
     </PageTemplate>
