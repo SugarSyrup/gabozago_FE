7e4ad6b49cbedf6c38dfdd27713a8e359ca7b5f9
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-03-03 15:37:39 +0900

    refactor(#49): 변경된 파일/폴더 구조 적용

---
+++ src/components/profile/TripItem/index.tsx
@@ -0,0 +1,29 @@
+import * as S from "./style";
+import MeatBallsMenuIcon from "../../../assets/icons/meatballsMenu.svg?react";
+import LocationIcon from "../../../assets/icons/location.svg?react";
+import CalendarIcon from "../../../assets/icons/calendar.svg?react";
+
+interface Props {
+    name:string;
+    location:string;
+    startDate: string;
+    endDate: string;
+}
+
+function TripItem({name, location, startDate, endDate}: Props) {
+    return(
+        <S.Container>
+            <S.ThumbnailWrapper></S.ThumbnailWrapper>
+            <S.Info>
+                <S.Name>{name}</S.Name>
+                <S.Desc><LocationIcon /> {location}</S.Desc>
+                <S.Desc><CalendarIcon /> {startDate} - {endDate}</S.Desc>
+            </S.Info>
+            <S.OptionWrapper>
+                <MeatBallsMenuIcon />
+            </S.OptionWrapper>
+        </S.Container>
+    )
+}
+
+export default TripItem;
