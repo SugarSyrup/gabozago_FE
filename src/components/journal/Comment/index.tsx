4c72f4de4fe75039308a63778319e43fe5636800
여다희 <yeodahui.dev@gmail.com>
2024-03-10 19:58:35 +0900

    feat(#56): 댓글 및 답글 UI 구현

--- src/components/journal/Comment/index.tsx
+++ src/components/journal/Comment/index.tsx
@@ -1,11 +1,23 @@
 import * as S from "./style";
 import Heading from "../../common/Heading";
 import SendIcon from "../../../assets/icons/send.svg?react";
+import CommentItem from "../CommentItem";
 
 interface Props {
   commentInputPosition?: "bottom" | "top";
 }
 function Comment({ commentInputPosition = "top" }: Props) {
+  const comment = {
+    id: "9de9f8ff-2a75-4294-Bf2f-7e06755395c8",
+    name: "상은수",
+    username: "user-wfd37gu",
+    profileImage: "https://placehold.co/100x100/png",
+    createDate: "2024-03-03/04:20:13",
+    like: 71,
+    text: "댓글내용",
+    parentCommentId: "",
+  };
+
   const comments: object[] | null = [];
   return (
     <S.Container>
@@ -26,7 +38,8 @@ function Comment({ commentInputPosition = "top" }: Props) {
         </S.SendButton>
       </S.CommentInputBox>
       <S.Contents>
-        {comments && comments.length !== 0 ? (
+        <CommentItem {...comment} replys={[comment, comment, comment]} />
+        {/* {comments && comments.length !== 0 ? (
           <>
             <div>댓글1</div>
             <div>댓글2</div>
@@ -37,7 +50,7 @@ function Comment({ commentInputPosition = "top" }: Props) {
             <p>아직 댓글이 없습니다.</p>
             <p>가장 먼저 댓글을 남겨 보세요!</p>
           </S.NoCommentBox>
-        )}
+        )} */}
       </S.Contents>
     </S.Container>
   );
---
+++ src/components/journal/CommentItem/style.ts
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export const CommentBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 6px;
+`;
+
+export const UserProfileImgBox = styled.div`
+  flex: 0 0 fit-content;
+  width: 34px;
+  height: 34px;
+  overflow: hidden;
+`;
+
+export const UserProfileImg = styled.img`
+  width: 27px;
+  height: 27px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+export const ContentsBox = styled.div`
+  flex: 1 1 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 6px;
+`;
+
+export const UserNameSpan = styled.span`
+  margin-right: 4px;
+
+  font-size: 10px;
+  font-weight: 600;
+`;
+export const TimestampSpan = styled.span`
+  font-size: 10px;
+  font-weight: 500;
+
+  color: ${({ theme }) => theme.gray01};
+`;
+
+export const CommentParagraph = styled.p`
+  margin-top: 6px;
+  margin-bottom: 3px;
+  width: 100%;
+  line-height: 14px;
+  font-size: 12px;
+  font-weight: 400;
+  word-break: keep-all;
+`;
+
+export const ActionBox = styled.div`
+  display: flex;
+  gap: 6px;
+`;
+
+export const IconButton = styled.button`
+  cursor: pointer;
+  padding: 0;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  color: ${({ theme }) => theme.gray01};
+  font-size: 10px;
+  border: 0px;
+  background-color: transparent;
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+export const ReplyBox = styled.div`
+  margin-top: 3px;
+  margin-left: 40px;
+`;
+
+export const ReplyToggleButton = styled.button`
+  padding: 0;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  cursor: pointer;
+  border: 0;
+  background-color: transparent;
+  color: ${({ theme }) => theme.gray};
+
+  font-size: 10px;
+  font-weight: 400;
+  line-height: 20px;
+
+  &::before {
+    display: inline-block;
+    content: "";
+    width: 20px;
+    height: 1px;
+    background-color: ${({ theme }) => theme.gray02};
+  }
+`;
+
+export const ReplyList = styled.ol`
+  padding: 15px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
