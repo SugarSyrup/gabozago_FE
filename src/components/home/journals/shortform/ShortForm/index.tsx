789476114287527dd69e7048f36b88169c323d77
여다희 <yeodahui.dev@gmail.com>
2024-03-13 15:55:00 +0900

    feat(#55): 숏폼 페이지 유저 및 영상 정보 박스 추가

--- src/components/home/journals/shortform/ShortForm/index.tsx
+++ src/components/home/journals/shortform/ShortForm/index.tsx
@@ -1,5 +1,9 @@
 import * as S from "./style";
-import { useRef } from "react";
+import { Link } from "react-router-dom";
+import { Suspense, useRef } from "react";
+import FollowBtn from "../../../../profile/FollowBtn";
+import UserIcon from "../../../../../assets/icons/user.svg?react";
+import LocationIcon from "../../../../../assets/icons/location.svg?react";
 
 export interface TShortForm {
   id: number;
@@ -14,7 +18,6 @@ export interface TShortForm {
   like: number;
   bookmark: number;
   commentCount: number;
-  rel: 0;
 }
 
 function ShortForm({
@@ -36,29 +39,48 @@ function ShortForm({
     autoplay: 1,
     controls: 0,
     loop: 1,
+    mute: 1,
     modestbranding: 1,
     fs: 0,
     playsinline: 0,
+    rel: 0,
   };
   const queryString = Object.entries(opts)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
 
-  setTimeout(() => {
-    youtubeRef.current?.click();
-    console.dir(youtubeRef.current);
-  }, 1000);
-
   return (
     <S.Container>
+      <S.InfoBox>
+        <p>
+          <Link to={`profile/${userid}`}>
+            {profileImage ? (
+              <UserIcon />
+            ) : (
+              <S.ProfileImage src={profileImage} alt="" />
+            )}
+            <span>{username}</span>
+          </Link>
+          <FollowBtn isFollowing={false} />
+        </p>
+        <p>{title}</p>
+        <p>
+          <span>
+            <LocationIcon />
+            {location}
+          </span>
+        </p>
+      </S.InfoBox>
+      {/* <S.ControlBox></S.ControlBox> */}
       <S.YoutubeContainer>
-        <S.YoutubeIframe
-          ref={youtubeRef}
-          src={`https://www.youtube.com/embed/${videoId}?${queryString}`}
-          loading="lazy"
-          allowFullScreen={false}
-          allow="autoplay"
-        />
+        <Suspense fallback={<S.YoutubeFallback>로딩 중...</S.YoutubeFallback>}>
+          <S.YoutubeIframe
+            ref={youtubeRef}
+            src={`https://www.youtube.com/embed/${videoId}?${queryString}`}
+            allow="autoplay"
+            loading="lazy"
+          />
+        </Suspense>
       </S.YoutubeContainer>
     </S.Container>
   );
--- src/components/home/journals/shortform/ShortForm/style.ts
+++ src/components/home/journals/shortform/ShortForm/style.ts
@@ -7,14 +7,107 @@ export const Container = styled.div`
   bottom: 0;
   left: 0;
 
-  transform: translate(0);
+  transform: translateY(0);
   transition: transform ease-in-out 0.3s;
   height: 100%;
+`;
+
+export const InfoBox = styled.div`
+  padding: 17px 20px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+
+  p:first-child {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    a {
+      display: flex;
+      align-items: center;
+      gap: 6px;
+
+      font-size: 14px;
+      font-weight: 500;
+      text-decoration: none;
+      color: ${({ theme }) => theme.white};
+
+      svg {
+        width: 30px;
+        path {
+          fill: ${({ theme }) => theme.white};
+        }
+      }
+    }
+
+    button {
+      font-size: 9px;
+      padding: 0 10px 0 6px;
+      line-height: 20px;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  }
+  p:nth-child(2) {
+    padding-left: 2px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+    color: ${({ theme }) => theme.white};
+  }
+  p:nth-child(3) {
+    display: flex;
+    gap: 20px;
+
+    font-size: 13px;
+    color: ${({ theme }) => theme.white};
+
+    span {
+      display: flex;
+      align-items: center;
+      line-height: 23px;
+    }
+
+    svg {
+      width: 16px;
+      height: 16px;
+      margin-right: 6px;
+      path {
+        fill: ${({ theme }) => theme.white};
+      }
+    }
+  }
+`;
+
+export const ProfileImage = styled.image`
+  width: 24px;
+  height: 24px;
+  object-fit: cover;
+`;
+
+export const FollowButton = styled.button`
+  border: 0;
+  background-color: ${({ theme }) => theme.main};
+  color: white;
 `;
 
 export const YoutubeContainer = styled.div`
   width: 100%;
   height: inherit;
+`;
+
+export const YoutubeFallback = styled.div`
+  width: 100%;
+  height: inherit;
+  background-color: ${({ theme }) => theme.black};
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const YoutubeIframe = styled.iframe`
