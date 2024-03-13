ac8e975a95f80185bb4697e34ede845ee1d79905
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-03-13 21:42:54 +0900

    feat(#57): 게시글/영상 바텀 Navigation 구현

---
+++ src/pages/mytrip/post/BottomNav/index.tsx
@@ -0,0 +1,43 @@
+import ClapIcon from "../../../../assets/icons/clap.svg?react";
+import CommentIcon from "../../../../assets/icons/comment.svg?react";
+import BookMarkIcon from "../../../../assets/icons/bookmark.svg?react";
+import ShareIcon from "../../../../assets/icons/share.svg?react";
+
+import * as S from "./style";
+
+interface Props {
+    postId: string;
+    isClap: boolean;
+    claps:number;
+    comment:number;
+    bookmark:number;
+    shares: number;
+}
+
+function BottomNav({postId, isClap, claps, comment, bookmark, shares}: Props) {
+    // TODO: 각 BottomNav 클릭시 action 추가
+    // TODO: 댓글은 rebase후 바로 연결
+
+    return(
+        <S.Navigation>
+            <S.NavigationItem>
+                <ClapIcon />
+                <span>{claps}</span>
+            </S.NavigationItem>
+            <S.NavigationItem>
+                <CommentIcon />
+                <span>{comment}</span>
+            </S.NavigationItem>
+            <S.NavigationItem>
+                <BookMarkIcon />
+                <span>{bookmark}</span>
+            </S.NavigationItem>
+            <S.NavigationItem>
+                <ShareIcon />
+                <span>{shares}</span>
+            </S.NavigationItem>
+        </S.Navigation>
+    )
+}
+
+export default BottomNav;
