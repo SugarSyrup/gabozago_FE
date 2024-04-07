216a39a9b6099e93b24a58a5ffe59402af765924
여다희 <yeodahui.dev@gmail.com>
2024-04-08 04:09:16 +0900

    feat(#55): 스냅사진 페이지 본문 내용 컴포넌트 구현

--- src/pages/journal/SnapshotPage/index.tsx
+++ src/pages/journal/SnapshotPage/index.tsx
@@ -1,15 +1,20 @@
 import * as S from "./style";
 import PageTemplate from "../../../components/common/PageTemplate";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { TSnapshot } from "../../../components/home/journals/snapshot/SnapshotList";
 import { snapshots } from "../../../components/home/journals/Journals";
 import BackButton from "../../../components/common/BackButton";
 import KebabMenuIcon from "../../../assets/icons/menu_kebab.svg?react";
+import userIcon from "../../../assets/icons/user.svg";
+import ClapIcon from "../../../assets/icons/clap.svg?react";
+import CommentIcon from "../../../assets/icons/comment.svg?react";
 import Comment from "../../../components/journal/Comment";
+import FollowBtn from "../../../components/profile/FollowBtn";
 
 function SnapshotPage() {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
   const id = Number(pathname.split("/")[pathname.split("/").length - 1]);
   const [data, setData] = useState<TSnapshot>(snapshots[0]);
 
@@ -31,7 +36,36 @@ function SnapshotPage() {
             <S.Image src={item} alt="" />
           ))}
         </S.CarouselContainer>
-        <S.InfoContainer>본문 영역</S.InfoContainer>
+        <S.InfoContainer>
+          <S.TopInfoBox>
+            <S.ProfileBox>
+              <S.ProfileImage
+                src={data.profileImage ? data.profileImage : userIcon}
+                onClick={() => {
+                  navigate(`/profile/${data.userid}`);
+                }}
+              />
+              <div>
+                <p>{data.username}</p>
+                <p>{data.createdAt}</p>
+              </div>
+            </S.ProfileBox>
+            <FollowBtn isFollowing={false} />
+          </S.TopInfoBox>
+          <S.ContentsBox>
+            <p>{data.text}</p>
+            <S.BottomInfoBox>
+              <S.InfoItem>
+                <ClapIcon />
+                {data.like}
+              </S.InfoItem>
+              <S.InfoItem>
+                <CommentIcon />
+                {data.commentCount}
+              </S.InfoItem>
+            </S.BottomInfoBox>
+          </S.ContentsBox>
+        </S.InfoContainer>
         <Comment id={id} commentInputPosition="top" />
       </S.ContentsContainer>
     </PageTemplate>
--- src/pages/journal/SnapshotPage/style.ts
+++ src/pages/journal/SnapshotPage/style.ts
@@ -68,3 +68,71 @@ export const Image = styled.img`
 export const InfoContainer = styled.div`
   padding: 18px 20px 15px;
 `;
+
+export const ProfileBox = styled.div`
+  display: flex;
+  div {
+    width: fit-content;
+    p {
+      text-decoration: none;
+      color: ${({ theme }) => theme.black};
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 17px;
+    }
+    p:nth-child(2) {
+      margin-top: 2px;
+      color: ${({ theme }) => theme.gray02};
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 14px;
+    }
+  }
+`;
+export const TopInfoBox = styled.div`
+  position: relative;
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+`;
+
+export const ProfileImage = styled.img`
+  margin-right: 13px;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+export const ContentsBox = styled.div`
+  width: 100%;
+  font-size: 14px;
+  line-height: 20px;
+  word-break: keep-all;
+
+  p {
+    word-break: keep-all;
+    color: ${({ theme }) => theme.gray01};
+    padding: 10px 0px 15px 45px;
+  }
+`;
+
+export const BottomInfoBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  gap: 9px;
+`;
+
+export const InfoItem = styled.span`
+  display: flex;
+  align-items: center;
+
+  color: ${({ theme }) => theme.gray};
+
+  svg {
+    width: 20px;
+    height: 20px;
+    margin-right: 6px;
+  }
+`;
