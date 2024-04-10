943ac9e03d3c2963023eaf18602045e4916c9a84
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-04-10 19:15:19 +0900

    fix(#62): 약관 data 배열 생성해서, map으로 출력되게 수정

--- src/components/auth/CheckBoxs/index.tsx
+++ src/components/auth/CheckBoxs/index.tsx
@@ -1,10 +1,34 @@
 import { useState } from "react";
 
 import * as S from "./style";
-import CheckBoxItem from "../../common/CheckBoxItem";
+import CheckBoxItem from "../../common/CheckBox";
+
+const terms = [
+    {
+        id:"ageCheck",
+        label: "만 14세 이상입니다.",
+        required: true,
+    },
+    {
+        id:"serviceCheck",
+        label: "서비스 이용약관 동의",
+        required: true,
+    },
+    {
+        id:"personalCheck",
+        label: "개인정보 수집 및 이용 동의",
+        required: true,
+    },
+    {
+        id:"eventCheck",
+        label: "이벤트 및 할인 혜택 안내 동의",
+        required: false,
+    }
+]
 
 function CheckBoxs() {
     const [allChecks, setAllChecks] = useState(false);
+
     return(
         <S.CheckBoxsContainer>
             <S.CheckBoxContainer>
@@ -25,39 +49,25 @@ function CheckBoxs() {
                 </CheckBoxItem>
             </S.CheckBoxContainer>
             <S.CheckBoxContainer>
-                <CheckBoxItem name="ageCheck" className="checkbox" inputId="ageCheck" required={true} onClick={(e) => {
-                    if(!e.currentTarget.checked) {
-                        setAllChecks(false);
-                    }
-                }}>
-                    <S.CheckBoxLabel htmlFor="ageCheck">만 14세 이상입니다.</S.CheckBoxLabel>
-                    <S.CheckBoxRequired>(필수)</S.CheckBoxRequired>
-                </CheckBoxItem>
+                {
+                    terms.map((term) => 
+                        <CheckBoxItem name={term.id} required={term.required} className="checkbox" onClick={(e) => {
+                            if(!e.currentTarget.checked) {
+                                setAllChecks(false);
+                            }
+                        }}>
+                            <S.CheckBoxLabel>{term.label}</S.CheckBoxLabel>
+                            {
+                                term.required ?
+                                <S.CheckBoxRequired>(필수)</S.CheckBoxRequired>
+                                :
+                                <S.CheckBoxNotRequired>(선택)</S.CheckBoxNotRequired>
+                            }
+                            
+                        </CheckBoxItem>
+                    )
+                }
                 
-                <CheckBoxItem name="serviceCheck" className="checkbox" inputId="serviceCheck" required={true} onClick={(e) => {
-                    if(!e.currentTarget.checked) {
-                        setAllChecks(false);
-                    }
-                }}>
-                    <S.CheckBoxLabelLink to="/">서비스 이용약관 동의</S.CheckBoxLabelLink>
-                    <S.CheckBoxRequired>(필수)</S.CheckBoxRequired>
-                </CheckBoxItem>
-                <CheckBoxItem name="personalCheck"  className="checkbox" required={true} onClick={(e) => {
-                    if(!e.currentTarget.checked) {
-                        setAllChecks(false);
-                    }
-                }}>
-                    <S.CheckBoxLabelLink to="/">개인정보 수집 및 이용 동의</S.CheckBoxLabelLink>
-                    <S.CheckBoxRequired>(필수)</S.CheckBoxRequired>
-                </CheckBoxItem>
-                <CheckBoxItem name="eventCheck"  className="checkbox" onClick={(e) => {
-                    if(!e.currentTarget.checked) {
-                        setAllChecks(false);
-                    }
-                }}>
-                    <S.CheckBoxLabel>이벤트 및 할인 혜택 안내 동의</S.CheckBoxLabel>
-                    <S.CheckBoxNotRequired>(선택)</S.CheckBoxNotRequired>
-                </CheckBoxItem>
             </S.CheckBoxContainer>
         </S.CheckBoxsContainer>
     )
