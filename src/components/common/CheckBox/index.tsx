8a1b9f7bdbc967c6f5cc985b2f04b507c255ecf5
여다희 <yeodahui.dev@gmail.com>
2024-04-16 12:22:00 +0900

    fix(#68): CheckBoxItem 컴포넌트 onClick 이벤트를 onChange 이벤트로 변경

--- src/components/common/CheckBox/index.tsx
+++ src/components/common/CheckBox/index.tsx
@@ -1,23 +1,39 @@
-import { MouseEventHandler } from "react";
+import { ChangeEventHandler } from "react";
 import * as S from "./style";
 
 interface Props {
-    checked?: boolean,
-    name?:string,
-    inputId?:string,
-    required?:boolean,
-    className?:string,
-    onClick?: MouseEventHandler<HTMLInputElement>,
-    children?: React.ReactNode
+  checked?: boolean;
+  name?: string;
+  inputId?: string;
+  required?: boolean;
+  className?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  children?: React.ReactNode;
 }
 
-function CheckBoxItem({checked, onClick, name, inputId, required, className, children}: Props) {
-    return(
-            <S.CheckBoxInputContainer>
-                <input type="checkbox" checked={checked} onClick={onClick} name={name} id={inputId} required={required} className={className}/>
-                {children}
-            </S.CheckBoxInputContainer>
-    )
+function CheckBoxItem({
+  checked,
+  onChange,
+  name,
+  inputId,
+  required,
+  className,
+  children,
+}: Props) {
+  return (
+    <S.CheckBoxInputContainer>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+        name={name}
+        id={inputId}
+        required={required}
+        className={className}
+      />
+      {children}
+    </S.CheckBoxInputContainer>
+  );
 }
 
 export default CheckBoxItem;
--- src/components/signUp/CheckBoxs/index.tsx
+++ src/components/signUp/CheckBoxs/index.tsx
@@ -4,73 +4,79 @@ import * as S from "./style";
 import CheckBoxItem from "../../common/CheckBox";
 
 const terms = [
-    {
-        id:"ageCheck",
-        label: "만 14세 이상입니다.",
-        required: true,
-    },
-    {
-        id:"serviceCheck",
-        label: "서비스 이용약관 동의",
-        required: true,
-    },
-    {
-        id:"personalCheck",
-        label: "개인정보 수집 및 이용 동의",
-        required: true,
-    },
-    {
-        id:"eventCheck",
-        label: "이벤트 및 할인 혜택 안내 동의",
-        required: false,
-    }
-]
+  {
+    id: "ageCheck",
+    label: "만 14세 이상입니다.",
+    required: true,
+  },
+  {
+    id: "serviceCheck",
+    label: "서비스 이용약관 동의",
+    required: true,
+  },
+  {
+    id: "personalCheck",
+    label: "개인정보 수집 및 이용 동의",
+    required: true,
+  },
+  {
+    id: "eventCheck",
+    label: "이벤트 및 할인 혜택 안내 동의",
+    required: false,
+  },
+];
 
 function CheckBoxs() {
-    const [allChecks, setAllChecks] = useState(false);
+  const [allChecks, setAllChecks] = useState(false);
 
-    return(
-        <S.CheckBoxsContainer>
-            <S.CheckBoxContainer>
-                <CheckBoxItem checked={allChecks} inputId="allCheck" onClick={(e) => {
-                    const checkboxs = document.getElementsByClassName("checkbox");
-                    for(let i = 0; i<checkboxs.length; i++) {
-                        if(e.currentTarget.checked){
-                            checkboxs[i].checked = true;
-                            setAllChecks(true);
-                        } else {
-                            checkboxs[i].checked = false;
-                            setAllChecks(false);
-                        }
-                    }
-                }}>
-                    <S.CheckBoxLabelHighlight htmlFor="allCheck" >약관 전체 동의합니다.</S.CheckBoxLabelHighlight>
-                    <S.CheckBoxNotRequired>선택항목 포함</S.CheckBoxNotRequired>
-                </CheckBoxItem>
-            </S.CheckBoxContainer>
-            <S.CheckBoxContainer>
-                {
-                    terms.map((term) => 
-                        <CheckBoxItem name={term.id} required={term.required} className="checkbox" onClick={(e) => {
-                            if(!e.currentTarget.checked) {
-                                setAllChecks(false);
-                            }
-                        }}>
-                            <S.CheckBoxLabel>{term.label}</S.CheckBoxLabel>
-                            {
-                                term.required ?
-                                <S.CheckBoxRequired>(필수)</S.CheckBoxRequired>
-                                :
-                                <S.CheckBoxNotRequired>(선택)</S.CheckBoxNotRequired>
-                            }
-                            
-                        </CheckBoxItem>
-                    )
-                }
-                
-            </S.CheckBoxContainer>
-        </S.CheckBoxsContainer>
-    )
+  return (
+    <S.CheckBoxsContainer>
+      <S.CheckBoxContainer>
+        <CheckBoxItem
+          checked={allChecks}
+          inputId="allCheck"
+          onChange={(e) => {
+            const checkboxs = document.getElementsByClassName("checkbox");
+            for (let i = 0; i < checkboxs.length; i++) {
+              if (e.currentTarget.checked) {
+                checkboxs[i].checked = true;
+                setAllChecks(true);
+              } else {
+                checkboxs[i].checked = false;
+                setAllChecks(false);
+              }
+            }
+          }}
+        >
+          <S.CheckBoxLabelHighlight htmlFor="allCheck">
+            약관 전체 동의합니다.
+          </S.CheckBoxLabelHighlight>
+          <S.CheckBoxNotRequired>선택항목 포함</S.CheckBoxNotRequired>
+        </CheckBoxItem>
+      </S.CheckBoxContainer>
+      <S.CheckBoxContainer>
+        {terms.map((term) => (
+          <CheckBoxItem
+            name={term.id}
+            required={term.required}
+            className="checkbox"
+            onClick={(e) => {
+              if (!e.currentTarget.checked) {
+                setAllChecks(false);
+              }
+            }}
+          >
+            <S.CheckBoxLabel>{term.label}</S.CheckBoxLabel>
+            {term.required ? (
+              <S.CheckBoxRequired>(필수)</S.CheckBoxRequired>
+            ) : (
+              <S.CheckBoxNotRequired>(선택)</S.CheckBoxNotRequired>
+            )}
+          </CheckBoxItem>
+        ))}
+      </S.CheckBoxContainer>
+    </S.CheckBoxsContainer>
+  );
 }
 
 export default CheckBoxs;
