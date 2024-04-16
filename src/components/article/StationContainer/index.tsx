c57c7ff66c4f6e0ac0e649e12abbd666aa4e79ee
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-04-17 03:52:16 +0900

    feat(#72): Station Props 설정

--- src/components/article/StationContainer/index.tsx
+++ src/components/article/StationContainer/index.tsx
@@ -2,27 +2,26 @@ import DoubleCircleIcon from "../../../assets/icons/double_circle.svg?react";
 
 import * as S from "./style";
 
-function StationContainer() {
+interface Props {
+    data: string[]
+}
+
+function StationContainer({data}: Props) {
     return(
             <S.StationList>
-                <S.StationItem>
-                    <S.Linker isFirst={true}>
-                        <DoubleCircleIcon />
-                    </S.Linker>
-                    <S.TextContainer>
-                        <S.StationNumber>Station 0</S.StationNumber>
-                        <S.StationName>진짜 로컬의 디저트는 무엇일까?</S.StationName>
-                    </S.TextContainer>
-                </S.StationItem>
-                <S.StationItem>
-                    <S.Linker isLast={true}>
-                        <DoubleCircleIcon />
-                    </S.Linker>
-                    <S.TextContainer isLast={true}>
-                        <S.StationNumber>Station 1</S.StationNumber>
-                        <S.StationName>기본과 함께 성장한 그녀의 브랜드</S.StationName>
-                    </S.TextContainer>
-                </S.StationItem>
+                {
+                    data.map((station, index) => 
+                        <S.StationItem>
+                            <S.Linker isFirst={index === 0} isLast={index+1===data.length}>
+                                <DoubleCircleIcon />
+                            </S.Linker>
+                            <S.TextContainer isLast={index+1===data.length}>
+                                <S.StationNumber>Station {index}</S.StationNumber>
+                                <S.StationName>{station}</S.StationName>
+                            </S.TextContainer>
+                        </S.StationItem>
+                    )
+                }
             </S.StationList>
     )
 }
