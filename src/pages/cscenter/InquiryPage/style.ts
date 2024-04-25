bb013313a974c79419fab7c5cd54af5de9cb5d65
여다희 <yeodahui.dev@gmail.com>
2024-04-26 08:21:00 +0900

    feat(#68): 서비스 문의하기 페이지 컴포넌트 구현

---
+++ src/pages/cscenter/InquiryPage/style.ts
@@ -0,0 +1,217 @@
+import styled, { css } from "styled-components";
+
+export const ButtonContainer = styled.div`
+  padding: 20px;
+  width: 100%;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 15px;
+`;
+
+export const TitleHeading = styled.h2`
+  color: ${({ theme }) => theme.black};
+
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+`;
+
+export const DescSpan = styled.span`
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 5px;
+
+  img {
+    width: 16px;
+    height: 16px;
+
+    border-radius: 100%;
+  }
+
+  word-break: keep-all;
+  color: ${({ theme }) => theme.gray02};
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0.2px;
+`;
+
+export const SelectButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 9px 15px;
+
+  cursor: pointer;
+  text-align: start;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0.5px;
+
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.white};
+  border: 1px solid ${({ theme }) => theme.gray04};
+
+  svg {
+    width: 20px;
+    height: 20px;
+    padding: 3px;
+  }
+`;
+
+export const SelectOptionList = styled.ul`
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+
+  li {
+    padding: 10px 20px;
+    cursor: pointer;
+    width: 100%;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 28px;
+
+    &:hover,
+    &:active {
+      background-color: ${({ theme }) => theme.blue05};
+    }
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 9px;
+
+  /* position:relative; */
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px 16px;
+
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.gray04};
+  background: ${({ theme }) => theme.white};
+
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0.2px;
+`;
+
+export const TextCountParagraph = styled.p`
+  width: 100%;
+  text-align: right;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 24px;
+  color: ${({ theme }) => theme.gray02};
+`;
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  min-height: 200px;
+  width: 100%;
+  padding: 10px 16px;
+
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.gray04};
+  background: ${({ theme }) => theme.white};
+
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0.2px;
+`;
+
+const fileBoxCSS = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 80px;
+  height: 80px;
+
+  cursor: pointer;
+  border-radius: 7px;
+  background: ${({ theme }) => theme.gray05};
+
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px;
+  letter-spacing: 0.5px;
+
+  color: ${({ theme }) => theme.gray02};
+
+  &:hover {
+    background: ${({ theme }) => theme.gray04};
+  }
+`;
+
+export const FileLabel = styled.label`
+  ${fileBoxCSS}
+
+  svg {
+    width: 28px;
+    height: 28px;
+  }
+`;
+
+export const FileList = styled.ol`
+  display: flex;
+  width: 100%;
+  gap: 10px;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+`;
+
+export const FileBox = styled.div`
+  ${fileBoxCSS}
+  padding: 8px;
+  position: relative;
+  background: ${({ theme }) => theme.gray07};
+  border: 1px solid ${({ theme }) => theme.gray05};
+
+  p {
+    width: 100%;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 4;
+    line-height: 16px;
+    text-overflow: ellipsis;
+    color: ${({ theme }) => theme.gary01};
+  }
+
+  &:hover::after {
+    content: "x";
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 20px;
+    height: 20px;
+    background-color: ${({ theme }) => theme.white};
+    border-radius: 50%;
+    color: ${({ theme }) => theme.gray02};
+    transform: translate(-5px, 5px);
+  }
+`;
