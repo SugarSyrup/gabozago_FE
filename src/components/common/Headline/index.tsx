e9f285b39490fe684930469f75e1a7af9c272297
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-04-29 17:56:02 +0900

    feat(#79) : 공통 컴포넌트 Headline 생성

---
+++ src/components/common/Headline/index.tsx
@@ -0,0 +1,19 @@
+import * as S from "./style";
+
+interface Props {
+    children: React.ReactNode;
+    size: "sm" | "md" | "lg";
+    color?: string;
+    noOfLine?: number;
+    maxWidth?: number;
+}
+
+function Headline({ children, size, noOfLine, maxWidth, color }: Props) {
+    return (
+        <S.Headline size={size} noOfLine={noOfLine} maxWidth={maxWidth}>
+            {children}
+        </S.Headline>
+    );
+}
+
+export default Headline;
