bcd4a06b1e923778fe7a597768b553dada64ff30
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-04-29 19:24:32 +0900

    feat(#79): 홈페이지 탭 바 텍스트 변경

--- src/pages/home/HomePage/index.tsx
+++ src/pages/home/HomePage/index.tsx
@@ -4,13 +4,14 @@ import PageTemplate from "../../../components/common/PageTemplate";
 import TabBar from "../../../components/common/TabBar";
 import SearchIcon from "../../../assets/icons/search.svg?react";
 import Journals from "../../../components/home/journals/Journals";
+import Recommendation from "../../../components/home/Recommendation";
 
 function HomePage() {
   const [focusedTabIndex, setFocusedTabIndex] = useState<number>(0);
   const tabs = [
-    { id: "추천", name: "추천", content: <>추천</> },
+    { id: "추천", name: "추천", content: <Recommendation /> },
     { id: "큐레이팅", name: "큐레이팅", content: <>큐레이팅</> },
-    { id: "후기", name: "후기", content: <Journals /> },
+    { id: "숏폼", name: "숏폼", content: <Journals /> },
   ];
 
   return (
