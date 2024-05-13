1a15d6275ee5779d7a06874fd43e5b9bce016815
여다희 <yeodahui.dev@gmail.com>
2024-05-13 21:32:00 +0900

    fix(#86): Typography 컴포넌트 word-break 속성 추가, Typography.Body 스타일컴포넌트 color 속성 누락 수정

--- src/components/common/Typography/Headline/style.ts
+++ src/components/common/Typography/Headline/style.ts
@@ -1,45 +1,47 @@
 import styled, { css } from "styled-components";
 
 interface Props {
-    size: "sm" | "md" | "lg";
-    noOfLine?: number;
-    maxWidth?: number;
-    color?: string;
+  size: "sm" | "md" | "lg";
+  noOfLine?: number;
+  maxWidth?: number;
+  color?: string;
 }
 
 export const Headline = styled.h1<Props>`
-    ${({theme, size, color}) => {
-        const COLOR = color ? color : theme.black;
+  ${({ theme, size, color }) => {
+    const COLOR = color ? color : theme.black;
 
-        switch(size) {
-            case "lg" :
-                return css`
-                    color: ${COLOR};
-                    font-size: 24px;
-                    font-weight: 600;
-                    line-height: 40px;
-                `
-            case "md" :
-                return css`
-                    color: ${COLOR};
-                    font-size: 22px;
-                    font-weight: 600;
-                    line-height: 36px;
-                `
-            case "sm" :
-                return css`
-                    color: ${COLOR};
-                    font-size: 20px;
-                    font-weight: 600;
-                    line-height: 32px;
-                `
-        }
-    }}
-    
-    max-width: ${({ maxWidth }) => (maxWidth ? `${maxWidth}px` : "100%")};
-    text-overflow: ellipsis;
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-line-clamp: ${({ noOfLine }) => (noOfLine ? noOfLine : 1)};
-    -webkit-box-orient: vertical;
+    switch (size) {
+      case "lg":
+        return css`
+          color: ${COLOR};
+          font-size: 24px;
+          font-weight: 600;
+          line-height: 40px;
+        `;
+      case "md":
+        return css`
+          color: ${COLOR};
+          font-size: 22px;
+          font-weight: 600;
+          line-height: 36px;
+        `;
+      case "sm":
+        return css`
+          color: ${COLOR};
+          font-size: 20px;
+          font-weight: 600;
+          line-height: 32px;
+        `;
+    }
+  }}
+
+  word-break: keep-all;
+  overflow-wrap: anywhere;
+  max-width: ${({ maxWidth }) => (maxWidth ? `${maxWidth}px` : "100%")};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: ${({ noOfLine }) => (noOfLine ? noOfLine : 1)};
+  -webkit-box-orient: vertical;
 `;
