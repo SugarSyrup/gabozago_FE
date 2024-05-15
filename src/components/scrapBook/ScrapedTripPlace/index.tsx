038dfd82ecd91e9872ef0446d963da2758a33e85
여다희 <yeodahui.dev@gmail.com>
2024-05-15 14:07:00 +0900

    feat(#86): 장소 스크랩에 필터 추가 및 UI 변경 반영

--- src/components/scrapBook/ScrapedTripPlace/index.tsx
+++ src/components/scrapBook/ScrapedTripPlace/index.tsx
@@ -1,51 +1,128 @@
 import * as S from "./style";
-import { useRecoilState } from "recoil";
-import { scrapedPlacesState } from "../../../recoil/scrapBook/scrapState";
 import BookMarkIcon from "../../../assets/icons/bookmark_filled.svg?react";
-import imageIcon from "../../../assets/icons/image.svg";
+import { useEffect, useState } from "react";
+import { get, post } from "../../../utils/api";
+import { useRecoilState } from "recoil";
+import {
+  TFilter,
+  journalFilterState,
+} from "../../../recoil/journals/journalState";
+import FilterList from "../../common/FilterList";
+import RightChevronIcon from "../../../assets/icons/chevron_right.svg?react";
+import Typography from "../../common/Typography";
+import { useNavigate } from "react-router-dom";
+
+interface Place {
+  id: number;
+  name: string;
+  theme: string[];
+  address: string;
+}
 
 function ScrapedTripPlace() {
-  const [places, setPlaces] = useRecoilState(scrapedPlacesState);
+  const navigate = useNavigate();
+  const [filter, setFilter] = useRecoilState<TFilter>(journalFilterState);
+  const [places, setPlaces] = useState<Place[]>([
+    {
+      id: 40,
+      name: "네이밍카페 커피고 난곡점",
+      theme: ["반려동물"],
+      address: "서울특별시 관악구 신림동 538-1",
+    },
+    {
+      id: 80,
+      name: "김밥천국",
+      theme: ["미식"],
+      address: "서울특별시 관악구 미성동 1680-51",
+    },
+  ]);
+
+  const getPlaces = async () => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      const { data } = await get<{
+        next: string | null;
+        previous: string | null;
+        results: Place[];
+      }>(`folder/scrap/place`, {
+        params: {
+          location: filter.location.join(","),
+        },
+      });
+      console.log(data);
+      setPlaces(data.results);
+
+      return;
+    }
+  };
+
+  const toggleBookmark = (id: number) => {
+    const token = localStorage.getItem("access_token");
+
+    if (token) {
+      post<{
+        next: string | null;
+        previous: string | null;
+        results: Place[];
+      }>(`folder/scrap/place`, {
+        placeId: id,
+      });
+
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getPlaces();
+  }, [filter]);
+
   return (
-    <S.PlaceList>
-      {places.map((item) => (
-        <S.PlaceItem>
-          <S.BookMarkButton
-            onClick={() => {
-              setPlaces((prev) => prev.filter((place) => place.id !== item.id));
-            }}
-          >
-            <BookMarkIcon />
-          </S.BookMarkButton>
-          <S.StyledLink to={`/place/${item.id}`}>
-            <S.PlaceInfoBox>
-              <S.TopInfoBox>
-                <S.PlaceNameSpan>{item.placeName}</S.PlaceNameSpan>
-                <S.PlaceThemeSpan>{item.theme}</S.PlaceThemeSpan>
-              </S.TopInfoBox>
-              <S.AddressParagraph>{item.address}</S.AddressParagraph>
-              <S.ImageList>
-                {item.images ? (
-                  item.images.map((url) => (
-                    <S.Image
-                      loading="lazy"
-                      decoding="async"
-                      src={url}
-                      alt={""}
-                    />
-                  ))
-                ) : (
-                  <S.ImagePlaceHolder
-                    src={imageIcon}
-                    alt={"이미지 불러오기에 실패했습니다."}
-                  />
-                )}
-              </S.ImageList>
-            </S.PlaceInfoBox>
-          </S.StyledLink>
-        </S.PlaceItem>
-      ))}
-    </S.PlaceList>
+    <>
+      <S.FilterContainer>
+        <FilterList
+          filters={["location"]}
+          filterState={filter}
+          filterSetState={setFilter}
+        />
+      </S.FilterContainer>
+      <S.PlaceList>
+        {places.map((item) => (
+          <S.PlaceItem>
+            <div>
+              <S.BookMarkButton
+                onClick={() => {
+                  toggleBookmark(item.id);
+                  setPlaces((prev) =>
+                    prev.filter((place) => place.id !== item.id)
+                  );
+                }}
+              >
+                <BookMarkIcon />
+              </S.BookMarkButton>
+              <S.StyledLink to={`/place/${item.id}`}>
+                <S.PlaceInfoBox>
+                  <S.TopInfoBox>
+                    <S.PlaceNameSpan>{item.name}</S.PlaceNameSpan>
+                    <S.PlaceThemeSpan>{item.theme}</S.PlaceThemeSpan>
+                  </S.TopInfoBox>
+                  <S.AddressParagraph>{item.address}</S.AddressParagraph>
+                </S.PlaceInfoBox>
+              </S.StyledLink>
+            </div>
+            <S.DetailViewButton
+              onClick={() => {
+                navigate(`/place/${item.id}`);
+              }}
+            >
+              <Typography.Label size="sm" color="#5276FA">
+                상세보기
+              </Typography.Label>
+              <RightChevronIcon />
+            </S.DetailViewButton>
+          </S.PlaceItem>
+        ))}
+      </S.PlaceList>
+    </>
   );
 }
 
