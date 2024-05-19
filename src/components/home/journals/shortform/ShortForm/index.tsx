10e674276b1b98f7ffea1b7a49d7d0cabb543a88
여다희 <yeodahui.dev@gmail.com>
2024-05-19 15:48:00 +0900

    feat(#104): 숏폼 장소 목록 버튼 추가

--- src/components/home/journals/shortform/ShortForm/index.tsx
+++ src/components/home/journals/shortform/ShortForm/index.tsx
@@ -10,8 +10,10 @@ import FilledBookMarkIcon from "../../../../../assets/icons/bookmark_filled_whit
 import CommentIcon from "../../../../../assets/icons/comment.svg?react";
 import LikeIcon from "../../../../../assets/icons/clap.svg?react";
 import FilledLikeIcon from "../../../../../assets/icons/clap_blue.svg?react";
+import PlaceIcon from "../../../../../assets/icons/place.svg?react";
 import ShareIcon from "../../../../../assets/icons/share.svg?react";
 import { post } from "../../../../../utils/api";
+import useModal from "../../../../../hooks/useModal";
 
 export interface TShortForm {
   id: number;
@@ -80,6 +82,22 @@ function ShortForm({
     playlist: videoId,
   };
   const [isContentOpened, setIsContentOpened] = useState<boolean>(false);
+  const {
+    Modal: PlacesModal,
+    modalOpen: placesModalOpen,
+    modalClose: placesModalClose,
+  } = useModal({
+    title: "",
+    handle: true,
+  });
+  const {
+    Modal: ScrapModal,
+    modalOpen: scrapModalOpen,
+    modalClose: scrapModalClose,
+  } = useModal({
+    title: "",
+    handle: true,
+  });
 
   const toggleClap = async () => {
     post<{ community: string; postId: number }>("/clap/community", {
@@ -92,13 +110,13 @@ function ShortForm({
       setClap((prev) => ({ count: prev.count + 1, isActive: true }));
     }
   };
-  const toggleBookmark = async () => {
+  const toggleBookmark = async (scrapFolderId: number | null = null) => {
     post<{ community: string; postId: number; scrapFolderId: number | null }>(
       "/folder/scrap/community",
       {
         community: "short-form",
         postId: id,
-        scrapFolderId: null,
+        scrapFolderId: scrapFolderId,
       }
     );
     if (bookmark.isActive) {
@@ -128,87 +146,102 @@ function ShortForm({
   }, []);
 
   return (
-    <S.Container
-      onScroll={() => {
-        console.log("scroll");
-      }}
-    >
-      <S.YoutubeContainer ref={containerRef}>
-        <Suspense fallback={<S.YoutubeFallback>로딩 중...</S.YoutubeFallback>}>
-          <YouTube
-            videoId={videoId}
-            ref={playerRef}
-            loading="lazy"
-            opts={{
-              width: "100%",
-              height: "100px",
-              playerVars: playerOpts,
-            }}
-            onEnd={() => {
-              playerRef.current?.resetPlayer();
-            }}
-            onReady={() => {
-              const player = playerRef.current?.getInternalPlayer();
-              player.setVolume(40);
+    <>
+      <PlacesModal>장소 모달</PlacesModal>
+      <ScrapModal>스크랩 모달</ScrapModal>
+      <S.Container
+        onScroll={() => {
+          console.log("scroll");
+        }}
+      >
+        <S.YoutubeContainer ref={containerRef}>
+          <Suspense
+            fallback={<S.YoutubeFallback>로딩 중...</S.YoutubeFallback>}
+          >
+            <YouTube
+              videoId={videoId}
+              ref={playerRef}
+              loading="lazy"
+              opts={{
+                width: "100%",
+                height: "100px",
+                playerVars: playerOpts,
+              }}
+              onEnd={() => {
+                playerRef.current?.resetPlayer();
+              }}
+              onReady={() => {
+                const player = playerRef.current?.getInternalPlayer();
+                player.setVolume(40);
 
-              videoPlayControl();
+                videoPlayControl();
+              }}
+            />
+          </Suspense>
+        </S.YoutubeContainer>
+        <S.InfoBox>
+          <p>
+            <Link to={`profile/${userid}`}>
+              {profileImage ? (
+                <UserIcon />
+              ) : (
+                <S.ProfileImage src={profileImage} alt="" />
+              )}
+              <span>{username}</span>
+            </Link>
+          </p>
+          <p>{title}</p>
+          <S.ContentBox
+            isOpened={isContentOpened}
+            onClick={() => {
+              setIsContentOpened((prev) => !prev);
             }}
-          />
-        </Suspense>
-      </S.YoutubeContainer>
-      <S.InfoBox>
-        <p>
-          <Link to={`profile/${userid}`}>
-            {profileImage ? (
-              <UserIcon />
-            ) : (
-              <S.ProfileImage src={profileImage} alt="" />
-            )}
-            <span>{username}</span>
-          </Link>
-        </p>
-        <p>{title}</p>
-        <S.ContentBox
-          isOpened={isContentOpened}
-          onClick={() => {
-            setIsContentOpened((prev) => !prev);
-          }}
-        >
-          {content.split("\n").map((line) => (
-            <p>{line}</p>
-          ))}
-        </S.ContentBox>
-        <S.BottomInfoContainer>
-          <span>
-            <LocationIcon />
-            {location}
-          </span>
-          {theme.length > 0 && (
+          >
+            {content.split("\n").map((line) => (
+              <p>{line}</p>
+            ))}
+          </S.ContentBox>
+          <S.BottomInfoContainer>
             <span>
-              <ThemeIcon />
-              {theme.join(", ")}
+              <LocationIcon />
+              {location}
             </span>
-          )}
-        </S.BottomInfoContainer>
-      </S.InfoBox>
-      <S.ControlBox>
-        <S.IconButton onClick={toggleClap}>
-          {clap.isActive ? <FilledLikeIcon /> : <LikeIcon />}
-          {clap.count}
-        </S.IconButton>
-        <S.IconButton onClick={modalOpen}>
-          <CommentIcon />
-          {commentCount}
-        </S.IconButton>
-        <S.IconButton onClick={toggleBookmark}>
-          {bookmark.isActive ? <FilledBookMarkIcon /> : <BookMarkIcon />}
-          {bookmark.count}
-        </S.IconButton>
-        <S.IconButton onClick={popupOpen}>
-          <ShareIcon />
-        </S.IconButton>
-      </S.ControlBox>
-    </S.Container>
+            {theme.length > 0 && (
+              <span>
+                <ThemeIcon />
+                {theme.join(", ")}
+              </span>
+            )}
+          </S.BottomInfoContainer>
+        </S.InfoBox>
+        <S.ControlBox>
+          <S.IconButton onClick={toggleClap}>
+            {clap.isActive ? <FilledLikeIcon /> : <LikeIcon />}
+            {clap.count}
+          </S.IconButton>
+          <S.IconButton onClick={modalOpen}>
+            <CommentIcon />
+            {commentCount}
+          </S.IconButton>
+          <S.IconButton
+            onClick={(e) => {
+              e.preventDefault();
+              toggleBookmark();
+              scrapModalOpen();
+            }}
+          >
+            {bookmark.isActive ? <FilledBookMarkIcon /> : <BookMarkIcon />}
+            {bookmark.count}
+          </S.IconButton>
+          <S.IconButton onClick={placesModalOpen}>
+            <PlaceIcon />
+          </S.IconButton>
+          <S.IconButton onClick={popupOpen}>
+            <ShareIcon />
+          </S.IconButton>
+        </S.ControlBox>
+      </S.Container>
+    </>
   );
 }
 
