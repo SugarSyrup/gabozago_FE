7269c103a580ab316378bb29f0a67c45889ddbb6
여다희 <yeodahui.dev@gmail.com>
2024-05-27 20:07:40 +0900

    fix(#110): 아티클-앵커 플로팅 버튼이 모달 위로 가는 문제 해결

--- src/components/article/StationContainer/index.tsx
+++ src/components/article/StationContainer/index.tsx
@@ -6,73 +6,81 @@ import useModal from "../../../hooks/useModal";
 import { useEffect } from "react";
 
 interface Props {
-    data: {
-        index: number,
-        name: string,
-        type: "station"
-    }[],
-    refs: React.MutableRefObject<null[] | HTMLDivElement[]>,
+  data: {
+    index: number;
+    name: string;
+    type: "station";
+  }[];
+  refs: React.MutableRefObject<null[] | HTMLDivElement[]>;
 }
 
-function StationContainer({data, refs}: Props) {
-    const {Modal, modalOpen, modalClose, isOpend} = useModal({});
+function StationContainer({ data, refs }: Props) {
+  const { Modal, modalOpen, modalClose, isOpend } = useModal({});
 
-    useEffect(() => {
-        console.log(data);
-    }, [])
+  useEffect(() => {
+    console.log(data);
+  }, []);
 
-    return(
-        <>
-            <S.ModalWrapper isOpen={isOpend}>
-                <Modal>
-                    <S.ModalTitle>Station 보기</S.ModalTitle>
-                    <S.StationList isBackground={false}>
-                        {
-                            data !== undefined && data.map((station, index) => 
-                                <S.StationItem>
-                                    <S.Linker isFirst={index === 0} isLast={index+1===data.length}>
-                                        <DoubleCircleIcon />
-                                    </S.Linker>
-                                    <S.TextContainer isLast={index+1===data.length} onClick={() => {
-                                        refs.current[index]?.scrollIntoView({
-                                            behavior: "smooth",
-                                        });
-                                        modalClose()
-                                    }}>
-                                        <S.StationNumber>Station {station.index}</S.StationNumber>
-                                        <S.StationName>{station.name}</S.StationName>
-                                    </S.TextContainer>
-                                </S.StationItem>
-                            )
-                        }
-                    </S.StationList>
-                </Modal>
-            </S.ModalWrapper>
-            <S.StationList isBackground={true}>
-                {
-                    data.map((station, index) => 
-                        <S.StationItem>
-                            <S.Linker isFirst={index === 0} isLast={index+1===data.length}>
-                                <DoubleCircleIcon />
-                            </S.Linker>
-                            <S.TextContainer isLast={index+1===data.length}  onClick={() => {
-                                    refs.current[index]?.scrollIntoView({
-                                      behavior: "smooth",
-                                    });
-                                    modalClose();
-                                }}>
-                                <S.StationNumber>Station {station.index}</S.StationNumber>
-                                <S.StationName>{station.name}</S.StationName>
-                            </S.TextContainer>
-                        </S.StationItem>
-                    )
-                }
-            </S.StationList>
-            <S.StationIcon onClick={() => {modalOpen()}}>
-                <BookIcon />
-            </S.StationIcon>
-        </>
-    )
+  return (
+    <>
+      <Modal>
+        <S.ModalTitle>Station 보기</S.ModalTitle>
+        <S.StationList isBackground={false}>
+          {data !== undefined &&
+            data.map((station, index) => (
+              <S.StationItem>
+                <S.Linker
+                  isFirst={index === 0}
+                  isLast={index + 1 === data.length}
+                >
+                  <DoubleCircleIcon />
+                </S.Linker>
+                <S.TextContainer
+                  isLast={index + 1 === data.length}
+                  onClick={() => {
+                    refs.current[index]?.scrollIntoView({
+                      behavior: "smooth",
+                    });
+                    modalClose();
+                  }}
+                >
+                  <S.StationNumber>Station {station.index}</S.StationNumber>
+                  <S.StationName>{station.name}</S.StationName>
+                </S.TextContainer>
+              </S.StationItem>
+            ))}
+        </S.StationList>
+      </Modal>
+      <S.StationList isBackground={true}>
+        {data.map((station, index) => (
+          <S.StationItem>
+            <S.Linker isFirst={index === 0} isLast={index + 1 === data.length}>
+              <DoubleCircleIcon />
+            </S.Linker>
+            <S.TextContainer
+              isLast={index + 1 === data.length}
+              onClick={() => {
+                refs.current[index]?.scrollIntoView({
+                  behavior: "smooth",
+                });
+                modalClose();
+              }}
+            >
+              <S.StationNumber>Station {station.index}</S.StationNumber>
+              <S.StationName>{station.name}</S.StationName>
+            </S.TextContainer>
+          </S.StationItem>
+        ))}
+      </S.StationList>
+      <S.StationIcon
+        onClick={() => {
+          modalOpen();
+        }}
+      >
+        <BookIcon />
+      </S.StationIcon>
+    </>
+  );
 }
 
-export default StationContainer
+export default StationContainer;
