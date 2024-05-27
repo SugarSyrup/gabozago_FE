abe41ea65509599018c5e141b439f66f6a4278f1
여다희 <yeodahui.dev@gmail.com>
2024-05-28 00:11:10 +0900

    fix(#0): 내여행 목록에서 지난 여행 클릭하면 일정 화면으로 이동

--- src/components/mytrip/MyLastScheduleCard/index.tsx
+++ src/components/mytrip/MyLastScheduleCard/index.tsx
@@ -8,58 +8,75 @@ import * as S from "./style";
 import { useNavigate } from "react-router-dom";
 import useMyTripModal from "../../../hooks/useMyTripModal";
 
-
 interface Props {
-    "id": number,
-    "title": string,
-    "departure_date": string
-    "arrival_date": string,
-    "regions": string[]
+  id: number;
+  title: string;
+  departure_date: string;
+  arrival_date: string;
+  regions: string[];
 }
 
 function MyLastScheduleCard({
-    id,
-    title,
-    departure_date,
-    arrival_date,
-    regions,
+  id,
+  title,
+  departure_date,
+  arrival_date,
+  regions,
 }: Props) {
-    const navigate = useNavigate();
-    const {MyTripModal, modalOpen, modalClose, isModalOpend} = useMyTripModal({
-        id: id,
-        title: title,
-        departureDate: departure_date,
-        arrivalDate: arrival_date,
-    })
+  const navigate = useNavigate();
+  const { MyTripModal, modalOpen, modalClose, isModalOpend } = useMyTripModal({
+    id: id,
+    title: title,
+    departureDate: departure_date,
+    arrivalDate: arrival_date,
+  });
 
-    return (
-        <>
-            <MyTripModal />
-            <S.Card>
-                <S.InfoContainer>
-                    <S.ThumbnailWrapper>
-                        <LogoSmallIcon />
-                    </S.ThumbnailWrapper>
-                    <S.TextContainer>
-                        <Typography.Title size="md" noOfLine={2}>{title}</Typography.Title>
-                        <S.Infos>
-                            <S.Info>
-                                <CalendarIcon />
-                                <Typography.Label size="md" color="#424242">{departure_date} ~ {arrival_date}</Typography.Label>
-                            </S.Info>
-                            <S.Info>
-                                <LocationIcon />
-                                {regions.map((region, idx) => <Typography.Label size="md" color="#424242">{region}{idx !== regions.length - 1 && ','}</Typography.Label>)}    
-                            </S.Info>
-                        </S.Infos>
-                    </S.TextContainer>
-                    <S.MenuIcon onClick={() => {modalOpen()}}>
-                        <KebabMenuIcon />
-                    </S.MenuIcon>
-                </S.InfoContainer>
-            </S.Card>
-        </>
-    );
+  return (
+    <>
+      <MyTripModal />
+      <S.Card>
+        <S.InfoContainer
+          onClick={() => {
+            navigate(`./${id}`);
+          }}
+        >
+          <S.ThumbnailWrapper>
+            <LogoSmallIcon />
+          </S.ThumbnailWrapper>
+          <S.TextContainer>
+            <Typography.Title size="md" noOfLine={2}>
+              {title}
+            </Typography.Title>
+            <S.Infos>
+              <S.Info>
+                <CalendarIcon />
+                <Typography.Label size="md" color="#424242">
+                  {departure_date} ~ {arrival_date}
+                </Typography.Label>
+              </S.Info>
+              <S.Info>
+                <LocationIcon />
+                {regions.map((region, idx) => (
+                  <Typography.Label size="md" color="#424242">
+                    {region}
+                    {idx !== regions.length - 1 && ","}
+                  </Typography.Label>
+                ))}
+              </S.Info>
+            </S.Infos>
+          </S.TextContainer>
+          <S.MenuIcon
+            onClick={(e) => {
+              e.stopPropagation();
+              modalOpen();
+            }}
+          >
+            <KebabMenuIcon />
+          </S.MenuIcon>
+        </S.InfoContainer>
+      </S.Card>
+    </>
+  );
 }
 
 export default MyLastScheduleCard;
