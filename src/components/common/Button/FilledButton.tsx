e1e39fa8ea7e6b850c8c58861066901a8a9d0e04
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-07-22 16:15:39 +0900

    feat(#185): forwardRef 설정

--- src/components/common/Button/FilledButton.tsx
+++ src/components/common/Button/FilledButton.tsx
@@ -1,10 +1,14 @@
+import { ComponentPropsWithRef, ElementType, forwardRef } from 'react';
 import Typography from '../Typography';
 import * as S from './FilledButtons.style';
 import Props from '@_types/Button.type';
 
-function FilledButton({ children, rounded, bgColor, ...props }: Props) {
+function FilledButton(
+  { children, rounded, bgColor, ...props }: Props,
+  ref: ComponentPropsWithRef<ElementType>['ref'],
+) {
   return (
-    <S.Button rounded={rounded} bgColor={bgColor} {...props}>
+    <S.Button rounded={rounded} bgColor={bgColor} {...props} ref={ref}>
       {typeof children === 'string' && (
         <Typography.Title size="lg" color="inherit">
           {children}
@@ -15,4 +19,5 @@ function FilledButton({ children, rounded, bgColor, ...props }: Props) {
   );
 }
 
-export default FilledButton;
+export default forwardRef(FilledButton);
+FilledButton.displayName = 'FilledButton';
