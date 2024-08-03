fed2a654a048aa74163a9cac9c247840c8caa553
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-08-04 01:36:00 +0900

    feat(#201): Outline Button 생성

---
+++ src/components/common/Button/OutlineButton.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithRef, ElementType, forwardRef } from 'react';
+import Typography from '../Typography';
+import * as S from './OutlineButton.style';
+import Props from '@_types/Button.type';
+
+function OutlineButton(
+  { children, ...props }: Props,
+  ref: ComponentPropsWithRef<ElementType>['ref'],
+) {
+  return (
+    <S.Button {...props} ref={ref}>
+      {typeof children === 'string' && (
+        <Typography.Title size="sm" color="inherit">
+          {children}
+        </Typography.Title>
+      )}
+      {typeof children !== 'string' && children}
+    </S.Button>
+  );
+}
+
+export default forwardRef(OutlineButton);
+OutlineButton.displayName = 'PopupButton';
