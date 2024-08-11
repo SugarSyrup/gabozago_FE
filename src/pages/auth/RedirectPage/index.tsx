770dbb6bc83fda587ca04e44984f68408aeb5835
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-08-12 00:22:26 +0900

    feat(#209): social login test

--- src/pages/auth/RedirectPage/index.tsx
+++ src/pages/auth/RedirectPage/index.tsx
@@ -1,26 +1,31 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { post } from '@_utils/api';
+import { get, post } from '@_utils/api';
 
 function RedirectPage() {
   const { type } = useParams();
 
   useEffect(() => {
     const code = new URL(document.location.toString()).searchParams.get('code');
+    console.log(code);
     switch (type) {
       case 'kakao':
-        post('', {
-          code,
-        }).then((response) => {
-          // response 값에 따라 로그인 핸들링 하기
+        get(`/user/kakao/callback/?code=${code}`).then((response) => {
+          console.log(response);
         });
         break;
 
       case 'naver':
+        get(`/user/naver/callback/?code=${code}`).then((response) => {
+          console.log(response);
+        });
         break;
 
       case 'google':
+        get(`/user/google/callback/?code=${code}`).then((response) => {
+          console.log(response);
+        });
         break;
 
       default:
