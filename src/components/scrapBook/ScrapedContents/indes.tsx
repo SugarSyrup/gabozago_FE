80ff826ac99f11595df1ca7e0c747016394df782
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-08-18 22:07:42 +0900

    feat(#216): TODO 정리

--- src/components/scrapBook/ScrapedContents/indes.tsx
+++ src/components/scrapBook/ScrapedContents/indes.tsx
@@ -2,8 +2,19 @@ import Typography from '@_common/Typography';
 import useSearchInput from '../../../hooks/useSearchInput';
 import InstagramIcon from '../../../assets/imgs/instagram_icon.png';
 import * as S from './style';
+import { useEffect, useState } from 'react';
+import { get } from '@_utils/api';
+
+interface ContentResponseType {
+  id: number;
+  title: string;
+  thumbnail: string;
+  socialType: string;
+  isWatched: boolean;
+}
 
 function ScrapedContents() {
+  const [data, setData] = useState<ContentResponseType[]>([]);
   const [inputRef, SearchInput] = useSearchInput({
     placeholder: '콘텐츠 를 검색해보세요.',
     onChange: () => {},
@@ -14,6 +25,11 @@ function ScrapedContents() {
     },
   });
 
+  useEffect(() => {
+    // @TODO: API 연동
+    // @TODO: 검색에 따라 결과 바꿔지게 수정
+  }, []);
+
   return (
     <>
       <S.SearchBarWrapper>
@@ -21,13 +37,16 @@ function ScrapedContents() {
       </S.SearchBarWrapper>
       <S.ContentsHeader>
         <Typography.Title size="md" color="inherit">
+          {/* TODO: data.length */}
           전체 <S.FontHighlight>123</S.FontHighlight>
         </Typography.Title>
+        {/* @TODO: 편집 모드 UI 및 버튼 기능 구성 */}
         <Typography.Title size="sm" color="inherit">
           <S.FontHighlight>편집하기</S.FontHighlight>
         </Typography.Title>
       </S.ContentsHeader>
       <S.ContentsContainer>
+        {/* @TODO: Item onClick */}
         <S.ContentItem>
           <S.ImgWrapper>
             <img src="https://via.placeholder.com/150" alt="content" />
