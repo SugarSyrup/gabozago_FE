43e9614d6ee016861796928e9862251b898a0228
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-08-20 15:44:04 +0900

    fix: Toast Design 수정

--- src/pages/Auth/LoginPage/index.tsx
+++ src/pages/Auth/LoginPage/index.tsx
@@ -17,6 +17,7 @@ import { LoginResponse } from '@_types/LoginResponse.type';
 import * as S from './style';
 import toast from 'react-hot-toast';
 import { Toast } from '@_common/Toast';
+import Typography from '@_common/Typography';
 
 interface ClientConfig {
   clientId: string;
@@ -140,13 +141,19 @@ function LoginPage() {
                     if (error.status === 400) {
                       toast.custom(() => (
                         <Toast>
-                          다른 소셜 이메일로 가입되어 있습니다. {error.response.data.error}로 로그인
-                          해주세요
+                          <Typography.Title size="md" color="white">
+                            다른 소셜 이메일로 가입되어 있습니다. {error.response.data.error}로
+                            로그인 해주세요
+                          </Typography.Title>
                         </Toast>
                       ));
                     } else {
                       toast.custom(() => (
-                        <Toast>로그인 실해팼습니다. 다시 로그인 시도해 주세요</Toast>
+                        <Toast>
+                          <Typography.Title size="md" color="white">
+                            로그인 실해팼습니다. 다시 로그인 시도해 주세요
+                          </Typography.Title>
+                        </Toast>
                       ));
                     }
                   });
