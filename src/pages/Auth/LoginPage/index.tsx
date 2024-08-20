8d50a3baf0aa0e5a5db2ec357a8970dcff2ec41b
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-08-20 15:36:18 +0900

    fix: 소셜로그인 에러 핸들링

--- src/pages/Auth/LoginPage/index.tsx
+++ src/pages/Auth/LoginPage/index.tsx
@@ -15,6 +15,8 @@ import { get } from '@_utils/api';
 import { LoginResponse } from '@_types/LoginResponse.type';
 
 import * as S from './style';
+import toast from 'react-hot-toast';
+import { Toast } from '@_common/Toast';
 
 interface ClientConfig {
   clientId: string;
@@ -110,8 +112,8 @@ function LoginPage() {
                 });
 
                 const res = await window.AppleID.auth.signIn();
-                get<LoginResponse>(`/user/apple/callback/?code=${res.authorization.code}`).then(
-                  (response) => {
+                get<LoginResponse>(`/user/apple/callback/?code=${res.authorization.code}`)
+                  .then((response) => {
                     if (response.data.status === 'ACTIVE') {
                       localStorage.setItem('access_token', response.data.access);
 
@@ -133,8 +135,21 @@ function LoginPage() {
                         `/signup?type=apple&email=${response.data.user_data?.email}&nickname=${response.data.user_data?.nickname}&code=${response.data.access}`,
                       );
                     }
-                  },
-                );
+                  })
+                  .catch((error) => {
+                    if (error.status === 400) {
+                      toast.custom(() => (
+                        <Toast>
+                          다른 소셜 이메일로 가입되어 있습니다. {error.response.data.error}로 로그인
+                          해주세요
+                        </Toast>
+                      ));
+                    } else {
+                      toast.custom(() => (
+                        <Toast>로그인 실해팼습니다. 다시 로그인 시도해 주세요</Toast>
+                      ));
+                    }
+                  });
               }}
             >
               <AppleIcon width={40} height={40} />
--- src/pages/Auth/RedirectPage/index.tsx
+++ src/pages/Auth/RedirectPage/index.tsx
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 import { get } from '@_utils/api';
 import { LoginResponse } from '@_types/LoginResponse.type';
+import toast from 'react-hot-toast';
+import { Toast } from '@_common/Toast';
 
 function RedirectPage() {
   const { type } = useParams();
@@ -11,29 +13,42 @@ function RedirectPage() {
   useEffect(() => {
     const code = new URL(document.location.toString()).searchParams.get('code');
 
-    get<LoginResponse>(`/user/${type}/callback/?code=${code}`).then((response) => {
-      if (response.data.status === 'ACTIVE') {
-        localStorage.setItem('access_token', response.data.access);
+    get<LoginResponse>(`/user/${type}/callback/?code=${code}`)
+      .then((response) => {
+        if (response.data.status === 'ACTIVE') {
+          localStorage.setItem('access_token', response.data.access);
 
-        if (window.GabozagoDev) {
-          window.GabozagoDev.postUUID({
-            code: response.data.user_data.uuid,
-          });
-        }
-        if (window.webkit) {
-          window.webkit.messageHandlers.GabozagoDev.callback.message({
-            action: 'postUUID',
-            code: response.data.user_data.uuid,
-          });
-        }
+          if (window.GabozagoDev) {
+            window.GabozagoDev.postUUID({
+              code: response.data.user_data.uuid,
+            });
+          }
+          if (window.webkit) {
+            window.webkit.messageHandlers.GabozagoDev.callback.message({
+              action: 'postUUID',
+              code: response.data.user_data.uuid,
+            });
+          }
 
-        navigate('/');
-      } else {
-        navigate(
-          `/signup?type=${type}&email=${response.data.user_data?.email}&nickname=${response.data.user_data?.nickname}&code=${response.data.access}`,
-        );
-      }
-    });
+          navigate('/');
+        } else {
+          navigate(
+            `/signup?type=${type}&email=${response.data.user_data?.email}&nickname=${response.data.user_data?.nickname}&code=${response.data.access}`,
+          );
+        }
+      })
+      .catch((error) => {
+        if (error.status === 400) {
+          toast.custom(() => (
+            <Toast>
+              다른 소셜 이메일로 가입되어 있습니다. {error.response.data.error}로 로그인 해주세요
+            </Toast>
+          ));
+        } else {
+          toast.custom(() => <Toast>로그인 실해팼습니다. 다시 로그인 시도해 주세요</Toast>);
+        }
+        navigate('/login');
+      });
   }, []);
 
   return <></>;
