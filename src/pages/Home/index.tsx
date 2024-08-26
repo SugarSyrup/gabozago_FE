793c07b229afca36242ea02e03a0349ed49bff45
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-08-26 14:28:35 +0900

    fix: callback test

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,6 +26,7 @@ import { TUserProfile } from '@_types/TUserProfile';
 function HomePage() {
   const navigate = useNavigate();
   const [username, setUsername] = useState<string>('');
+  const [data, setData] = useState<string[]>([]);
 
   useEffect(() => {
     if (isLogin()) {
@@ -48,6 +49,34 @@ function HomePage() {
       >
         로컬 스토리지 삭제
       </button>
+      <button
+        type="button"
+        onClick={() => {
+          try {
+            if (window.GabozagoDev) {
+              window.GabozagoDev.postUUID({
+                code: response.data.user_data.uuid,
+              });
+            }
+            if (
+              window.webkit.messageHandlers.GabozagoDev &&
+              window.webkit.messageHandlers.GabozagoDev.callback
+            ) {
+              window.webkit.messageHandlers.GabozagoDev.callback({
+                action: 'postUUID',
+                code: response.data.user_data.uuid,
+              });
+            }
+          } catch (e) {
+            setData((prev) => [...prev, `${e}`]);
+          }
+        }}
+      >
+        콜백 테스트
+      </button>
+      {data.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
       <S.Header>
         <LogoIcon />
         <S.BellWrapper
