79dc9e4eb094f5ea3adf9d4a101a835736fb6d47
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-09-11 19:30:54 +0900

    feat(#287): 위치정보 이용약관 페이지 생성

--- src/pages/Profile/Terms/style.ts
+++ src/pages/Profile/Terms/style.ts
@@ -1,15 +1,50 @@
 import styled from 'styled-components';
 
-export const Heading = styled.h2`
-  margin: 20px 0 10px;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: normal;
+export const Contents = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 26px;
 `;
-export const Text = styled.p`
-  margin: 10px 0;
-  font-weight: 400;
-  font-size: 13px;
-  line-height: 22px;
-  color: ${({ theme }) => theme.gray01};
+
+export const LocationAccessToggleWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ToggleSwitch = styled.div<{ isActive: boolean }>`
+  width: 40px;
+  height: 24px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  position: relative;
+
+  background-color: ${({ isActive, theme }) =>
+    isActive ? theme.colors.blue.primary : theme.colors.gray.tertiary};
+
+  transition: all 0.3 linear;
+  div {
+    width: 18px;
+    height: 18px;
+    margin-top: 3px;
+    border-radius: 100%;
+    background-color: white;
+
+    position: absolute;
+    left: ${({ isActive }) => !isActive && '3px'};
+    right: ${({ isActive }) => isActive && '3px'};
+  }
+`;
+
+export const TermsLink = styled.span`
+  margin-top: -20px;
+  color: ${({ theme }) => theme.colors.blue.primary};
+  text-decoration: underline;
+  cursor: pointer;
 `;
