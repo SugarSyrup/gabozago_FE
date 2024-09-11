2f61067398ef79b2a54398927658ffb9e5357efa
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-09-11 19:21:26 +0900

    feat(#287): 푸시 알람 토클 설정

--- src/pages/Profile/SettingsPage/style.ts
+++ src/pages/Profile/SettingsPage/style.ts
@@ -5,10 +5,15 @@ export const ContentsWrapper = styled.div`
 
   display: flex;
   flex-direction: column;
-  gap: 12px;
   width: calc(100% + 40px);
   margin-left: -20px;
+`;
+
+export const SeperateLine = styled.div`
   background-color: ${({ theme }) => theme.gray05};
+  width: 100%;
+  height: 12px;
+  flex-shrink: 0;
 `;
 
 export const UserSettingButton = styled.button`
@@ -55,13 +60,15 @@ export const UserSettingLeftItems = styled.div`
 
 export const SettingsContainer = styled.div`
   height: 100%;
-  padding: 20px 20px;
   padding-bottom: 0px;
 
   display: flex;
   flex-direction: column;
-  gap: 10px;
+`;
 
+export const SettingListContainer = styled.div`
+  width: 100%;
+  padding: 20px 20px 10px;
   background-color: ${({ theme }) => theme.white};
 `;
 
@@ -102,4 +109,50 @@ export const LeaveButton = styled.button`
   border: 0;
   background-color: transparent;
   color: ${({ theme }) => theme.gray02};
+`;
+
+export const LocationAccessContainer = styled.div`
+  padding: 20px 20px 10px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 12px;
+
+  span {
+    color: #727272;
+  }
+`;
+
+export const LocationAccessToggleWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ToggleSwitch = styled.div<{ isActive: boolean }>`
+  width: 40px;
+  height: 24px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  position: relative;
+
+  background-color: ${({ isActive, theme }) =>
+    isActive ? theme.colors.blue.primary : theme.colors.gray.tertiary};
+
+  transition: all 0.3 linear;
+  div {
+    width: 18px;
+    height: 18px;
+    margin-top: 3px;
+    border-radius: 100%;
+    background-color: white;
+
+    position: absolute;
+    left: ${({ isActive }) => !isActive && '3px'};
+    right: ${({ isActive }) => isActive && '3px'};
+  }
 `;
