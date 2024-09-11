48af18969c3fdf8e1e0ea1ba604ab6687d66f70f
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-09-11 19:57:37 +0900

    feat(#287): 회원가입 로직 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -186,7 +186,7 @@ function HomePage() {
 
         <OutlineButton
           onClick={() => {
-            navigate('/aritcles');
+            navigate('/articles');
           }}
         >
           <S.ButtonContent>
