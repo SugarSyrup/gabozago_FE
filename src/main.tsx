057fe3a57f5ac3b94361cf3cbeef8f1fc246afee
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-09-13 01:34:36 +0900

    feat: GA4 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
@@ -8,6 +9,28 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { APIProvider as GoogleMapAPIProvider } from '@vis.gl/react-google-maps';
 import { GlobalFont, GlobalStyle, theme } from './styles';
 import router from './router';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_Project_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_Storage_Bucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_Messaging_Seder_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_Measurement_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const queryClient = new QueryClient();
 
