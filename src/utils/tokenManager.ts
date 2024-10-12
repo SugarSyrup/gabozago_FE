b29bdc40e646cee20e9340d0973933d6aaab12c6
김종진(SugarSyrup) <tlfvm04@naver.com>
2024-10-12 21:07:41 +0900

    fix(#324): refresh 로직 연결

--- src/utils/tokenManager.ts
+++ src/utils/tokenManager.ts
@@ -1,12 +1,22 @@
 import axios from 'axios';
+import { post } from './api';
 
 const TokenManager = (() => {
   let isRefreshing = false;
   let refreshSubscribers = [];
   let tokenExpirationTime;
 
+  const getIsRefreshing = () => {
+    return isRefreshing;
+  };
+
+  const pushRefreshSubscriber = (item) => {
+    refreshSubscribers.push(item);
+  };
+
   const setTokenExpirationTime = (expiresIn) => {
-    tokenExpirationTime = Date.now() + expiresIn * 1000;
+    const expiresInDate = new Date(expiresIn);
+    tokenExpirationTime = expiresInDate.getTime();
   };
 
   const isTokenExpired = () => {
@@ -14,17 +24,15 @@ const TokenManager = (() => {
   };
 
   const refreshToken = async () => {
-    // 실제 토큰 갱신 로직을 여기에 구현
-    // 예: return await axios.post('/api/refresh-token');
-    return axios.post('/user/jwt-token-auth/refresh');
+    return post('user/jwt-token-auth/refresh');
   };
 
   const logout = () => {
     // 로그아웃 로직 구현
     // 예: 토큰 삭제, 상태 초기화 등
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('refresh_token');
-    window.location.href = '/login';
+    // localStorage.removeItem('access_token');
+    // localStorage.removeItem('refresh_token');
+    // window.location.href = '/login';
   };
 
   const refreshTokenAndRetry = async (error) => {
@@ -32,10 +40,12 @@ const TokenManager = (() => {
       if (!isRefreshing) {
         isRefreshing = true;
         const response = await refreshToken();
-        const newToken = response.data.accessToken;
-        const { expiresIn } = response.data;
-        setTokenExpirationTime(expiresIn);
+        const newToken = response.data.access;
+        const { access_expires_at } = response.data;
+        setTokenExpirationTime(access_expires_at);
         isRefreshing = false;
+        localStorage.setItem('access_token', newToken);
+
         refreshSubscribers.forEach((cb) => cb(newToken));
         refreshSubscribers = [];
       }
@@ -46,54 +56,13 @@ const TokenManager = (() => {
     }
   };
 
-  // 요청 인터셉터
-  axios.interceptors.request.use(
-    async (config) => {
-      console.log('a');
-      if (isTokenExpired()) {
-        await refreshTokenAndRetry({ config });
-      }
-      return config;
-    },
-    (error) => Promise.reject(error),
-  );
-
-  // 응답 인터셉터
-  axios.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-      const originalRequest = error.config;
-      if (
-        error.response &&
-        error.response.status === 401 &&
-        // error.response.data.message === 'Token expired' &&
-        error.response.data.messages[0].message === 'Token is invalid or expired' &&
-        !originalRequest._retry
-      ) {
-        originalRequest._retry = true;
-        const retryOriginalRequest = new Promise((resolve) => {
-          refreshSubscribers.push((token) => {
-            originalRequest.headers.Authorization = `Bearer ${token}`;
-            resolve(axios(originalRequest));
-          });
-        });
-        if (!isRefreshing) {
-          try {
-            await refreshTokenAndRetry(error);
-          } catch (refreshError) {
-            return Promise.reject(refreshError);
-          }
-        }
-        return retryOriginalRequest;
-      }
-      return Promise.reject(error);
-    },
-  );
-
   return {
+    getIsRefreshing,
     setTokenExpirationTime,
-    refreshToken,
+    refreshTokenAndRetry,
     logout,
+    pushRefreshSubscriber,
+    isTokenExpired,
   };
 })();
 
